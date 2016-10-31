493715da6cf525cc03bdef756f54bfb6d2fdf389
Xmetal <xmetalfanx@yahoo.com>
2016-10-31 04:15:34 -0400

    Initial commit

--- linuxTheming.sh
+++ linuxTheming.sh
@@ -1,6 +1,39 @@
-baseFunctions= . /functions/f_themes.cfg
-$baseFunctions
+#!/bin/bash
 
+readonly basedir=$(pwd)
+rootdir=$basedir
+
+fThemes= . "$rootdir"/functions/f_themes.cfg
+$fThemes
+
+fPManager= . "$rootdir"/functions/f_packageManager.cfg
+$fPManager
+
+
+###############################################################
 
 ## get the distro used, calling quid's script
-getDistro
+checkDistro
+echo $distro
+
+
+    echo -e "Xmetal's Linux Theming Script \n"
+    echo -e "Please Select your choice \n"
+
+    echo -e   "1. \t Numix Themes"
+    echo -e   "2. \t Numix Icon Themes"
+
+    echo -e   "q. \t Exit to Prompt \n\n"
+    read -p   "Your Choice?:" themeSelection
+
+case $themeSelection in
+
+  1) numixThemes ;;
+
+  2) numixIcon ;;
+
+  q) clear
+      echo -e "Goodbye"
+      exit
+
+esac
