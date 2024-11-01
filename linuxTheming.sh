185cb4642222b955f06a00430decff327380be75
Xmetalfanx <Xmetalfanx@yahoo.com>
2024-11-01 15:19:14 -0400

    updated main menus

--- linuxTheming.sh
+++ linuxTheming.sh
@@ -26,63 +26,56 @@ while [ "$x" = "1" ]; do
 
     echo -e "Please Select your choice \n"
 
-    echo -e   "1. \t SUB-MENU - PPAs and Ubuntu Related"
-    echo -e   "2. \t SUB-MENU - MAC/Apple Related "
-    echo -e   "3. \t SUB-MENU - Numix Themes and Icon Submenu"
-    echo -e   "4. \t SUB-MENU - Manjaro-fy (Batch Manjaro Theme & Icon Install)"
+    echo -e   "1. \t SUB-MENU - MAC/Apple Related "
+    echo -e   "2. \t SUB-MENU - Numix Themes and Icon Submenu"
+    echo -e   "3. \t SUB-MENU - Manjaro-fy (Batch Manjaro Theme & Icon Install)"
 
-    echo -e   "5. \t Mint-Y Theme and Icon Packs"
-    echo -e   "6. \t Arc Theme and Icon Pack"
-    echo -e   "7. \t Windows 10 GTK3 Theme"
-    echo -e   "8. \t vimiDark GTK Theme"
-    echo -e   "9. \t vimiLight GTK Theme"
+    echo -e   "4. \t Mint-Y Theme and Icon Packs"
+    echo -e   "5. \t Arc Theme and Icon Pack"
+    echo -e   "6. \t Windows 10 GTK3 Theme"
+    echo -e   "7. \t vimiDark GTK Theme"
+    echo -e   "8. \t vimiLight GTK Theme"
 
     echo -e   "q. \t Exit to Prompt \n\n"
     read -r -p   "Your Choice?:" themeSelection
 
 case $themeSelection in
 
-  1)  bash $rootdir/submenus/ppaThemes.sh
-      x=1 
-      ;;
 
-  2)  bash $rootdir/submenus/macRelated.sh
+  1)  bash $rootdir/submenus/macRelated.sh
       x=1
       ;;
 
-  3) bash $rootdir/submenus/numix.sh
+  2) bash $rootdir/submenus/numix.sh
      x=1
       ;;
 
-  4) manjarofy
+  3) manjarofy
      x=1
       ;;
 
-  5) MintyThemes
+  4) MintyThemes
      MintyIcons
      echo "installed Mint-Y"
      x=1
      ;;
 
-  6)  gitArcThemes
-      gitArcIcons
-      gitArcOpenBox
-      echo "installed Arc"
+  5)  
+     
       x=1
       ;;
 
-  7) win10 
+  6) win10 
     x=1
     ;;
 
-  8) vimiDarkGTK 
+  7) vimiDarkGTK 
     x=1
     ;;
 
-  9) vimiLightGTK 
+  8) vimiLightGTK 
       x=1
       ;;
-
 
   t) read -r -p "Please enter a URL:" testURL
 
